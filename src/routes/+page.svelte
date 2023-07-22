e0083e9c2625db051d26d88159f1d2e4972a876c
Hcastro1515m <Elmenolx8@gmail.com>
2023-07-21 23:47:24 -0400

    Audio feature added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,25 @@
 	let dictionaryEntry: DictionaryEntry[] | null = null;
 	let isLoading: boolean = false;
 	let error: string = '';
+	let audioElement: HTMLAudioElement | null = null;
+	let isPlaying: boolean = false;
+
+	const playAudio = () => {
+		if (audioElement) {
+			audioElement.play();
+			isPlaying = true;
+		}
+	};
+
+	const pauseAudio = () => {
+		if (audioElement) {
+			audioElement.pause();
+			isPlaying = false;
+		}
+	};
 
 	const getWordDefinition = async () => {
+		isLoading = true;
 		try {
 			const response: AxiosResponse<DictionaryEntry[]> = await axios.get(
 				`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
@@ -33,10 +50,22 @@
 				dictionaryEntry = null;
 				error = 'An error occurred while fetching the definition.';
 			}
+		} finally {
+			isLoading = false;
 		}
 	};
 
+	// Listen for the audio 'ended' event to reset the button state when the audio finishes playing.
+	const handleAudioEnded = () => {
+		isPlaying = false;
+	};
+
 	onMount(() => {
+		// Add an event listener for the 'ended' event of the audio element.
+		if (audioElement) {
+			audioElement.addEventListener('ended', handleAudioEnded);
+		}
+
 		const cachedResult = window.localStorage.getItem('dictionaryEntry');
 		if (cachedResult) {
 			dictionaryEntry = JSON.parse(cachedResult);
@@ -48,6 +77,7 @@
 	<form action="" on:submit|preventDefault={getWordDefinition}>
 		<div class="bg-gray-400 flex justify-between items-center rounded-lg relative">
 			<input
+				tabindex="0"
 				class="w-full p-4 bg-gray-300 font-bold dark:bg-slate-600 dark:placeholder:text-white rounded-lg outline-purple-500 pr-10"
 				type="search"
 				placeholder="Enter a word to search...."
@@ -62,9 +92,41 @@
 		</div>
 	</form>
 
-	<div class="mt-9">
+	<div class="mt-9 relative">
 		{#if dictionaryEntry}
 			{#each dictionaryEntry as entry}
+				{#if entry.phonetics[0].audio}
+					<div
+						class="absolute right-3 top-3 bg-purple-300 grid place-items-center p-5 rounded-full cursor-pointer"
+					>
+						{#if isPlaying}
+							<span
+								class="material-symbols-outlined"
+								role="button"
+								tabindex="1"
+								aria-label="Pause audio"
+								on:click={pauseAudio}
+								on:keydown={(event) => event.key === 'Enter' && pauseAudio()}
+							>
+								pause
+							</span>
+						{:else}
+							<span
+								role="button"
+								on:click={playAudio}
+								tabindex="0"
+								on:keydown={(event) => event.key === 'Enter' && playAudio()}
+								aria-label="word definition"
+								class="material-symbols-outlined"
+							>
+								play_arrow
+							</span>
+						{/if}
+						<audio bind:this={audioElement}>
+							<source src={entry.phonetics[0].audio} />
+						</audio>
+					</div>
+				{/if}
 				<h1 class="text-5xl font-semibold first-letter:uppercase">{entry.word}</h1>
 				<p class="text-purple-500 mt-4 font-semibold">{entry.phonetic}</p>
 
@@ -81,7 +143,8 @@
 						<p class="font-light text-gray-800 dark:text-white mb-3 mt-10">
 							Synonyms
 							{#if meanings.synonyms}
-								<span class="text-purple-500 ml-10 first-letter:uppercase font-semibold">{meanings.synonyms}</span
+								<span class="text-purple-500 ml-10 first-letter:uppercase font-semibold"
+									>{meanings.synonyms}</span
 								>
 							{/if}
 						</p>
@@ -91,7 +154,13 @@
 				{#each entry.sourceUrls as source}
 					<div class="mt-5">
 						<hr />
-						<p class="font-light text-gray-800 dark:text-white mb-3">Source <a href={source} target="_blank" class="ml-10 underline text-purple-500 font-semibold">${source}</a></p>
+						<p class="font-light text-gray-800 dark:text-white mb-3">
+							Source <a
+								href={source}
+								target="_blank"
+								class="ml-10 underline text-purple-500 font-semibold">${source}</a
+							>
+						</p>
 					</div>
 				{/each}
 			{/each}
